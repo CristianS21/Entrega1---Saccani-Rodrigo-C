35366e2ed59a4017446dd44643d14b54e24f27a6
CristianSaccani <csaccani86@gmail.com>
2022-09-07 23:35:52 -0300

    casi listo

--- django_/app_agenda/urls.py
+++ django_/app_agenda/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('mascotas/', views.mascotas, name="Mascota"),
     path('plantas/', views.plantas, name="Plantas"),  
     path('form_mascotas/', views.formulario_mascota, name="form_mascotas"),
-    path('form_plantas/', views.formulario_plantas, name="form_plantas"),    
+    path('form_plantas/', views.formulario_plantas, name="form_plantas"),
+    #path('busc_mascota/', views.busc_mascota),
+    #path('buscar/', views.buscar, name="buscar")    
 ]
--- django_/app_agenda/views.py
+++ django_/app_agenda/views.py
@@ -12,30 +12,27 @@ def usuario (request):
     return render (request, "app_agenda/plantilla_1.html")
 
 def mascotas (request):
-    return render (request, "app_agenda/plantilla_2.html")
+    mascotas = Mascota.objects.all()
+    return render (request, "app_agenda/plantilla_2.html",{"mascotas":mascotas})
+
+def plantas (request):
+    plantas = Planta.objects.all()
+    return render (request, "app_agenda/plantilla_3.html",{"plantas":plantas})
 
 
 def formulario_mascota (request):
-    print ("1")
     if request.method =='POST':
         formulario=form_mascotas(request.POST)
-        print ("2")
         if formulario.is_valid():
             data = formulario.cleaned_data       
 
             mascota1 = Mascota (nombre= data ['nombre'],especie= data ['especie'],sexo= data ['sexo'], fecha_de_nacimiento= data ['fecha_de_nacimiento'])
             mascota1.save()
             return render (request, "app_agenda/plantilla_inicio.html")
-            
     else:
-        print ("3")
         formulario= form_mascotas()
     return render (request, "app_agenda/form_mascota.html",{"formulario":formulario})
 
-
-def buscar_mascota (request):
-    mascotas= Mascota.objects.all()
-    return render (request,"app_agenda/plantilla_2.html", {"mascotas":mascotas})
 
 
 
@@ -59,5 +56,4 @@ def buscar_planta (request):
 
 
 
-def plantas (request):
-    return render (request, "app_agenda/plantilla_3.html")
+
