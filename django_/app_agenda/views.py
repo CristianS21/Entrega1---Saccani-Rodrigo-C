80c952f8a49d6ffb48e1bb29a7441a4e321312d0
CristianSaccani <csaccani86@gmail.com>
2022-09-18 01:13:56 -0300

    limpiar usuario. probar funcionalidades de mascotas, plantas, regisro, login y logout

--- django_/app_agenda/views.py
+++ django_/app_agenda/views.py
@@ -11,30 +11,28 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
 
- 
-# Create your views here.
 def inicio (request):
     return render (request, "app_agenda/plantilla_inicio.html")
-
+@login_required
 def usuario (request):
     return render (request, "app_agenda/plantilla_1.html")
 
 # VIEWS de MASCOTAS
-
+@login_required
 def mascotas (request):
     mascotas = Mascota.objects.all()
     contexto= {"mascotas":mascotas}
     borrado= request.GET.get("borrado",None)
     contexto ["borrado"] = borrado    
     return render (request, "app_agenda/plantilla_2.html", contexto)
-
+@login_required
 def eliminar_item_mascota (request,nombre):
     mascota=Mascota.objects.get(nombre=nombre)
     borrado_nombre= mascota.nombre
     mascota.delete()
     url_final= f"{reverse ('Mascota')}?borrado={borrado_nombre}"
     return redirect (url_final)
-
+@login_required
 def formulario_mascota (request):
     if request.method =='POST':
         formulario=form_mascotas(request.POST)
@@ -46,10 +44,10 @@ def formulario_mascota (request):
     else:
         formulario= form_mascotas()
     return render (request, "app_agenda/form_mascota.html",{"formulario":formulario})
-
+@login_required
 def busqueda_mascotas (request):
     return render (request, "app_agenda/busqueda_mascotas.html")
-
+@login_required
 def buscar_mascotas (request):
     if request.GET["nombre"]:
         nombre=request.GET["nombre"]
@@ -58,7 +56,7 @@ def buscar_mascotas (request):
     else: 
         respuesta= "Error, no enviaste formulario"
     return render (request, "app_agenda/buscar_m_error.html",{"respuesta":respuesta} )
-    
+@login_required    
 def editar_item_mascota (request, nombre):
     mascota_edit = Mascota.objects.get(nombre=nombre)
 
@@ -86,14 +84,14 @@ def editar_item_mascota (request, nombre):
     return render (request, "app_agenda/form_mascota.html", {"formulario": formulario})
 
 #VIEWS de PLANTAS
-
+@login_required
 def plantas (request):
     plantas = Planta.objects.all()
     contexto= {"plantas":plantas}
     borrado= request.GET.get("borrado",None)
     contexto ["borrado"] = borrado    
     return render (request, "app_agenda/plantilla_3.html",contexto)
-
+@login_required
 def eliminar_item_planta (request,especie):
     planta=Planta.objects.get(especie=especie)
     borrado_especie= planta.especie
@@ -101,7 +99,7 @@ def eliminar_item_planta (request,especie):
     url_final= f"{reverse ('Plantas')}?borrado={borrado_especie}"
   
     return redirect (url_final)
-
+@login_required
 def formulario_plantas (request):
     if request.method =='POST':
         formulario=form_plantas(request.POST)
@@ -115,10 +113,10 @@ def formulario_plantas (request):
     else:
         formulario= form_plantas()
     return render (request, "app_agenda/form_plantas.html",{"formulario":formulario})
-
+@login_required
 def busqueda_plantas (request):
     return render (request, "app_agenda/busqueda_plantas.html")
-
+@login_required
 def buscar_plantas (request):
     if request.GET["especie"]:
         especie=request.GET["especie"]
@@ -127,7 +125,7 @@ def buscar_plantas (request):
     else: 
         respuesta= "Error, no enviaste formulario"
     return render (request, "app_agenda/buscar_p_error.html",{"respuesta":respuesta} ) 
-
+@login_required
 def editar_item_planta (request, especie):
     planta_edit = Planta.objects.get(especie=especie)
 
@@ -180,4 +178,4 @@ def registro (request):
     return render(request, "app_agenda/registro.html", {"form":formulario})
 
 class CustomLogoutView(LogoutView):
-    template_name = 'app_agenda/plantilla_inicio.html' 
+    template_name = 'app_agenda/logout.html' 
