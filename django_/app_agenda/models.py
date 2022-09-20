367a66e87cc87bd9716d65034da8bb55294af3ef
CristianSaccani <csaccani86@gmail.com>
2022-09-20 11:02:07 -0300

    gitignore

--- django_/app_agenda/models.py
+++ django_/app_agenda/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Usuario (models.Model):
     nombre= models.CharField(max_length=30)
     apellido= models.CharField(max_length=30)
     dni= models.IntegerField()
     email=models.EmailField()
-
-
+    
 class Mascota (models.Model):
     nombre= models.CharField(max_length=30)
     especie= models.CharField(max_length=30)
@@ -18,4 +19,13 @@ class Mascota (models.Model):
 class Planta (models.Model):
     especie= models.CharField(max_length=30)
     fecha_de_adopcion= models.DateField()
+
+class Avatar(models.Model):
+    # Vinculo con el usuario
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    # Subcaperta avatares de media :)
+    imagen = models.ImageField(upload_to='avatares', null=True, blank = True)
+
+    def __str__(self):
+        return f"Imagen de: {self.user}"
 
